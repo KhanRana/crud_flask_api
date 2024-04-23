878344a405ee52d74250b82bab2373a0cbc3eb7d
KhanRana <47982799+KhanRana@users.noreply.github.com>
2024-04-23 13:38:51 +0100

    create function that will add item to the list

--- app.py
+++ app.py
@@ -1,19 +1,23 @@
 from flask import Flask, render_template, request, redirect
 
 # create a flask app
-app =  Flask(__name__)
+app = Flask(__name__)
 
 
-items = [] # store items in memory list instead a DB
+items = []  # store items in memory list instead a DB
 
 
 # render the home page
 @app.route('/')
 def index():
-    return render_template("index.html", items=items )
+    return render_template("index.html", items=items)
 
-
-# 
+#
+@app.route("/create", methods=["POST"])
+def create():
+    name = request.form["name"]
+    items.append(name)
+    return redirect('/')
 
 if __name__ == "__main__":
     app.run()
