0b25342a0dd8d7fdd60a7e37b9f3fa19ab34a1c7
KhanRana <47982799+KhanRana@users.noreply.github.com>
2024-06-18 22:13:31 +0100

    remove always fail test

--- tests/test_crud_tests.py
+++ tests/test_crud_tests.py
@@ -1,40 +1,40 @@
 import requests
 import json
 
-# Test that always pass
-def test_always_passes ():
-    assert True
-    print ("Good, it always passes")
 
-# Test that always fail
-def test_always_fails ():
-    assert False
-    print ("Good, it always fails")
+# Test that always pass
+def test_always_passes():
+    assert True
+    print("Good, it always passes")
+
 
 # Test the /status endpoint
-def test_status ():
+def test_status():
     response = requests.get("http://127.0.0.1:5000")
     assert response.status_code == 200
-    print ("Good, the /status endpoint is working")
+    print("Good, the /status endpoint is working")
 
 
-def test_list_of_products ():
+# Get all products
+def test_list_of_products():
     response = requests.get("http://127.0.0.1:5000/")
     assert response.status_code == 200
-    print ("Good, the host/ endpoint is working")
+    print("Good, the host/ endpoint is working")
+
 
 # Test the /create endpoint
-def test_create ():
-    # act 
+def test_create():
+    # act
     data = {"name": "Evaline"}
     # arrange
     response = requests.post("http://127.0.0.1:5000/create", data=data)
     # assert
     assert response.status_code == 200
-    print ("Good, the /create endpoint is working")
+    print("Good, the /create endpoint is working")
+
 
 # Test the /update endpoint
-def test_update ():
+def test_update():
     # act
     data = {"old_name": "Evaline",
             "new_name": "Test Updated"}
@@ -43,14 +43,15 @@ def test_update ():
 
     # assert
     assert response.status_code == 200
-    print ("Good, the /update endpoint is working")
+    print("Good, the /update endpoint is working")
+
 
 # Test the /delete endpoint
-def test_delete ():
+def test_delete():
     # act
     data = {"name": "Test Updated"}
     # arrange
     response = requests.post("http://127.0.0.1:5000/delete", data=data)
     # assert
     assert response.status_code == 200
-    print ("Good, the /delete endpoint is working")
+    print("Good, the /delete endpoint is working")
