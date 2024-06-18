59231c1c43138f5877df95d5731898a553ebb773
KhanRana <47982799+KhanRana@users.noreply.github.com>
2024-06-18 12:17:06 +0100

    create tests for the api endpoints

--- .unittests/tests.py
+++ .unittests/tests.py
@@ -11,3 +11,46 @@ def test_always_fails ():
     assert False
     print ("Good, it always fails")
 
+# Test the /status endpoint
+def test_status ():
+    response = requests.get("http://127.0.0.1:5000")
+    assert response.status_code == 200
+    print ("Good, the /status endpoint is working")
+
+
+def test_list_of_products ():
+    response = requests.get("http://127.0.0.1:5000/")
+    assert response.status_code == 200
+    print ("Good, the host/ endpoint is working")
+
+# Test the /create endpoint
+def test_create ():
+    # act 
+    data = {"name": "Evaline"}
+    # arrange
+    response = requests.post("http://127.0.0.1:5000/create", data=data)
+    # assert
+    assert response.status_code == 200
+    print ("Good, the /create endpoint is working")
+
+# Test the /update endpoint
+def test_update ():
+    # act
+    data = {"old_name": "Test",
+            "new_name": "Test Updated"}
+    # arrange
+    response = requests.post("http://127.0.0.1:5000/update", data=data)
+
+    # assert
+    assert response.status_code == 200
+    print ("Good, the /update endpoint is working")
+
+# Test the /delete endpoint
+def test_delete ():
+    # act
+    data = {"name": "Test"}
+    # arrange
+    response = requests.delete("http://127.0.0.1:5000/update", data=data)
+    # assert
+    assert response.status_code == 200
+    print ("Good, the /delete endpoint is working")
