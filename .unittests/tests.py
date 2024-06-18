75373d13933a07e9637f5e3ae61b790385a94689
KhanRana <47982799+KhanRana@users.noreply.github.com>
2024-06-18 10:55:02 +0100

    create unit tests

---
+++ .unittests/tests.py
@@ -0,0 +1,13 @@
+import requests
+import json
+
+# Test that always pass
+def test_always_passes ():
+    assert True
+    print ("Good, it always passes")
+
+# Test that always fail
+def test_always_fails ():
+    assert False
+    print ("Good, it always fails")
+
