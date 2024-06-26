d91c8b9b3f0c6dcb0b0cdb5629d8786a3d346640
KhanRana <47982799+KhanRana@users.noreply.github.com>
2024-06-26 11:13:59 +0100

    comment and format the document

--- tests/test_crud_tests.py
+++ tests/test_crud_tests.py
@@ -9,20 +9,24 @@ def test_always_passes():
 
 # Check the status
 def test_status_code():
+    # act
     response = requests.get(f"{host}/status")
+    # assert
     assert response.status_code == 200
 
 # Get all products
 def test_list_of_products():
+    # act
     response = requests.get(f"{host}")
+    # assert
     assert response.status_code == 200
 
 
 # Test the /create endpoint
 def test_create():
-    # act
-    data = {"name": "Evaline"}
     # arrange
+    data = {"name": "Evaline"}
+    # act
     response = requests.post(f"{host}/create", data=data)
     # assert
     assert response.status_code == 200
@@ -30,10 +34,10 @@ def test_create():
 
 # Test the /update endpoint
 def test_update():
-    # act
+    # arrange
     data = {"old_name": "Evaline",
             "new_name": "Test Updated"}
-    # arrange
+    # act
     response = requests.post(f"{host}/update", data=data)
 
     # assert
@@ -42,9 +46,9 @@ def test_update():
 
 # Test the /delete endpoint
 def test_delete():
-    # act
-    data = {"name": "Test Updated"}
     # arrange
+    data = {"name": "Test Updated"}
+    # act
     response = requests.post(f"{host}/delete", data=data)
     # assert
     assert response.status_code == 200
